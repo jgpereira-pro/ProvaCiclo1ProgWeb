f86e9ea13dc23f8e07b460ea55d1ed95977eaf07
cavera <jgpereirafreitas@gmail.com>
2025-10-13 21:46:38 -0300

    tentandowesd

--- src/main/java/com/senac/joaog/repository/AtendenteRepository.java
+++ src/main/java/com/senac/joaog/repository/AtendenteRepository.java
@@ -4,10 +4,12 @@ import com.senac.joaog.entity.Atendente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface AtendenteRepository extends JpaRepository<Atendente, Integer> {
 
     @Query("SELECT a FROM Atendente a WHERE a.status >=0")
--- src/main/java/com/senac/joaog/repository/ChamadaAtendenteRepository.java
+++ src/main/java/com/senac/joaog/repository/ChamadaAtendenteRepository.java
@@ -2,9 +2,11 @@ package com.senac.joaog.repository;
 
 import com.senac.joaog.entity.ChamadaAtendente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ChamadaAtendenteRepository extends JpaRepository<ChamadaAtendente, Integer> {
     List<ChamadaAtendente> findByAtendenteId(int atendenteId);
 }
